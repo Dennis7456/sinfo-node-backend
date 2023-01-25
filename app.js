1689258ec8c7fc46b9ad4fbf10b8f372d40ecd39
Dennis7456 <denniskiplangat.dk@gmail.com>
2023-01-25 03:35:54 +0300

    small changes

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
 
 //static
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 //morgan logs
 app.use(morgan('dev'));
